872230e4f402a0b9a0c03e0facb7ea3b30e0a4f9
megabytis <166828632+megabytis@users.noreply.github.com>
2025-08-12 16:34:34 +0530

    update
    added Deletion, nd other advance operations like  count documents with filter , estimate count , index operations etc...

--- 02-Database/05-creating_DB.js
+++ 02-Database/05-creating_DB.js
@@ -27,20 +27,31 @@ async function callMe() {
   // Now i'll add documents/data to my database
   const collection = db.collection("data-1");
 
-  // Inserting a Document
-  const insertDoc = await collection.insertMany([
-    {
-      name: "Prachi Sucharita",
-      Age: 22,
-      College: "ITER, SOA, BBSR",
-      Course: "B.Tech CS",
-    },
-  ]);
+  // ✨✨✨ CRUD (Create, Read, Update, Delete) operation ✨✨✨
 
+  // 1️⃣ CREATE
+  // Inserting Document
+  // const insertDoc = await collection.insertMany([
+  //   {
+  //     name: "Prachi Sucharita",
+  //     Age: 22,
+  //     College: "ITER, SOA, BBSR",
+  //     Course: "B.Tech CS",
+  //     email: "ps123@gmail.com",
+  //   },
+  //   {
+  //     name: "Karan Samal",
+  //     Age: 20,
+  //     College: "Utkal University, Bhubaneswar",
+  //     Course: "BBA",
+  //     email: "karan@gmail.com",
+  //   },
+  // ]);
+
+  // 2️⃣ READ
   // Finding all Documents
   const findResult = await collection.find({}).toArray();
   console.log("Document found :- \n", findResult);
-  JSON.stringify;
 
   // Finding documents with Query Filter
   const xyz = await collection.find({ Course: "BBA" }).toArray();
@@ -49,6 +60,7 @@ async function callMe() {
     `\nFound Documents filtered by { Course: "BBA" } : \n${filteredDocs}`
   );
 
+  // 3️⃣ UPDATE
   // Updating a Document
   const updateDoc = await collection.updateOne(
     {
@@ -61,6 +73,37 @@ async function callMe() {
       },
     }
   );
+
+  // 4️⃣ DELETE
+  // const deleteDoc = await collection.deleteOne({
+  //   College: "Utkal University, BBSR",
+  // });
+
+  // ✨✨✨ Other Opeartions ✨✨✨
+
+  // 1️⃣ ADVANCE QUERY OPERATIONS
+  // Count all Documents
+  const countDoc = await collection.countDocuments({});
+  console.log("Documents found: ", countDoc);
+
+  // Count all Documents with filter
+  const countDocWithFilt = await collection.countDocuments({
+    Age: { $gt: 20 },
+    // ⚠️⚠️⚠️ Important ⚠️⚠️⚠️
+    // here '$gt' is an Query operator, which Matches values that are greater than a specified value.
+    // Query Operators list :- https://www.mongodb.com/docs/manual/reference/operator/query/
+  });
+  console.log("Counted with Filter : ", countDocWithFilt);
+
+  // Estimate Count (faster for large collections)
+  const estdc = await collection.estimatedDocumentCount();
+  console.log("Estimate count: ", estdc);
+
+  // 2️⃣ INDEX OPERATIONS
+  // Index management
+  await collection.createIndex({ email: 1 });
+  const indexes = await collection.listIndexes().toArray();
+  console.log(indexes);
 
   return "everything's now ready";
 }
