532dc833c8aec47bda24c311877a4e908c4d34fb
megabytis <166828632+megabytis@users.noreply.github.com>
2025-08-19 22:11:40 +0530

    added : multiple route handlers

--- 03-project-devtinder/devTinder-BACKEND/src/app.js
+++ 03-project-devtinder/devTinder-BACKEND/src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3292;
 
+// Server Listener
+app.listen(port, () => console.log("Server is running ...."));
+
 // Request Handler
 // Request handlers using route
 // GET
@@ -24,5 +27,51 @@ app.delete("/secret", (req, res) => {
   res.send("Data deleted successfully");
 });
 
-// Server Listener
-app.listen(port, () => console.log("Server is running ...."));
+// #############################
+// using Multiple Route Handlers
+// #############################
+
+app.get(
+  "/sendHere",
+  (req, res, next) => {
+    // res.send("Response-1");
+    next();
+  },
+  (req, res, next) => {
+    res.send("Response-2");
+  }
+);
+
+// #############################################
+// Using multiple route-handlers inside an array
+// #############################################
+
+app.get("/callMe", [
+  (req, res, next) => {
+    next();
+  },
+  (req, res, next) => {
+    res.send("Multiple route-handlers in array");
+  },
+]);
+
+// ####################################################################################
+// Using multiple route-handlers inside an array + Then after again more route handlers
+// ####################################################################################
+
+app.get(
+  "/moreRoutes",
+  [
+    (req, res, next) => {
+      next();
+    },
+    (req, res, next) => {
+      next();
+    },
+  ],
+  (req, res) => {
+    res.send(
+      "Response from route-handers in array & then again route handlers"
+    );
+  }
+);
